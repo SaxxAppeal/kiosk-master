215d0da3c1f44e9c45b4f8c9b384869d7101a339
Imraan <iakhan2@mac01-sas3138.math.ncsu.edu>
2022-04-28 16:42:09 -0400

    Updates

---
+++ precache-manifest.14b6a3f8743f14ab74c39f2f800739d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c61eb8d2567dd792f40",
+    "url": "/kiosk-master/static/css/main.9a84199f.chunk.css"
+  },
+  {
+    "revision": "9c61eb8d2567dd792f40",
+    "url": "/kiosk-master/static/js/main.9f455392.chunk.js"
+  },
+  {
+    "revision": "d1a10092b167e75f8eae",
+    "url": "/kiosk-master/static/js/runtime~main.5adc4759.js"
+  },
+  {
+    "revision": "7de752ca6c3d5b9bf609",
+    "url": "/kiosk-master/static/css/2.0e3dc5af.chunk.css"
+  },
+  {
+    "revision": "7de752ca6c3d5b9bf609",
+    "url": "/kiosk-master/static/js/2.2a45a455.chunk.js"
+  },
+  {
+    "revision": "36bd09361bf12f2db86f9281684b0c9b",
+    "url": "/kiosk-master/index.html"
+  }
+];
